8601d4b6fd77a44588c706b50977a18c394e8d74
David Monaghan <github@gangplankhq.com`>
2017-07-19 20:15:51 -0700

    Fix profile page

--- web/src/store/users/reducers.js
+++ web/src/store/users/reducers.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import Immutable from 'immutable';
 import { createReducer } from 'redux-immutablejs';
 
-import { FETCH_USERS_COMPLETE } from './actions';
+import { FETCH_USERS_COMPLETE, FETCH_USER_COMPLETE } from './actions';
 
 
 const INITIAL_STATE = new Immutable.Map();
@@ -14,8 +14,15 @@ function handleFetchUsersComplete(state, action) {
   }
   return state.merge(_.keyBy(action.payload.results, 'id'));
 }
+function handleFetchUserComplete(state, action) {
+  if (action.error) {
+    return state;
+  }
+  return state.set(action.payload.id, Immutable.fromJS(action.payload));
+}
 
 
 export default createReducer(INITIAL_STATE, {
   [FETCH_USERS_COMPLETE]: handleFetchUsersComplete,
+  [FETCH_USER_COMPLETE]: handleFetchUserComplete,
 });
