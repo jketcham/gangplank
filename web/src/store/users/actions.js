af0ce690f791c9e7f4162453a35d5994691db6d7
Jack Ketcham <jack@hiringsolved.com>
2017-08-04 01:26:27 -0700

    Fix/clean up update user epic; correctly use and catch ajax errors

--- web/src/store/users/actions.js
+++ web/src/store/users/actions.js
@@ -1,14 +1,14 @@
 import { createAction } from 'redux-actions';
 
-// TODO: add error action creator
 
-/* fetch users */
+/** fetch users */
 const FETCH_USERS_PENDING = 'FETCH_USERS_PENDING';
 const FETCH_USERS_COMPLETE = 'FETCH_USERS_COMPLETE';
 const FETCH_USERS_ERROR = 'FETCH_USERS_ERROR';
 
 const fetchUsers = createAction(FETCH_USERS_PENDING);
 const fetchUsersComplete = createAction(FETCH_USERS_COMPLETE);
+const fetchUsersError = createAction(FETCH_USERS_ERROR);
 
 /** fetch user */
 const FETCH_USER_PENDING = 'FETCH_USER_PENDING';
@@ -19,7 +19,7 @@ const fetchUser = createAction(FETCH_USER_PENDING);
 const fetchUserComplete = createAction(FETCH_USER_COMPLETE);
 const fetchUserError = createAction(FETCH_USER_ERROR);
 
-// update user
+/** update user */
 const UPDATE_USER_PENDING = 'UPDATE_USER_PENDING';
 const UPDATE_USER_COMPLETE = 'UPDATE_USER_COMPLETE';
 const UPDATE_USER_ERROR = 'UPDATE_USER_ERROR';
@@ -27,6 +27,7 @@ const UPDATE_USER_ERROR = 'UPDATE_USER_ERROR';
 const updateUser = createAction(UPDATE_USER_PENDING);
 const updateUserComplete = createAction(UPDATE_USER_COMPLETE);
 const updateUserError = createAction(UPDATE_USER_ERROR);
+
 
 export {
   FETCH_USERS_COMPLETE,
@@ -43,6 +44,7 @@ export {
   fetchUserError,
   fetchUsers,
   fetchUsersComplete,
+  fetchUsersError,
   updateUser,
   updateUserComplete,
   updateUserError,
