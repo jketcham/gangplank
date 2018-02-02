23e90ae84270709a0c468aa0adca3281fcea9c53
Jack Ketcham <jack@hiringsolved.com>
2018-02-01 21:26:18 -0700

    Add celerybeat setup

--- gangplank/celery.py
+++ gangplank/celery.py
@@ -1,20 +1,25 @@
 """
 Module to setup Celery worker.
 """
+from datetime import timedelta
 
 from celery import Celery
 
 from gangplank.config import config
 
-
 celery = Celery('gangplank')
 celery.conf.update(
-    # Default to using database number 10 so we don't conflict with the session
-    # store.
     BROKER_URL=config.get('BROKER_URL', 'amqp://rabbit'),
     CELERY_ACCEPT_CONTENT=['json'],
     CELERY_TASK_SERIALIZER='json',
     CELERY_IMPORTS=(
         'gangplank.tasks.email',
+        'gangplank.tasks.event_reminder',
     ),
+    CELERYBEAT_SCHEDULE={
+        'send-event-reminders': {
+            'task': 'gangplank.tasks.event_reminder.send_event_reminders',
+            'schedule': timedelta(hours=1)
+        },
+    },
 )
